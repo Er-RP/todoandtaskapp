856e03a4f42409f6ae35b602471996bbdafb527c
Rajapandi <rajapandi12121997@gmail.com>
2023-12-03 03:11:41 +0530

    cors fix 2

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,6 @@
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { ApolloServer } from '@apollo/server';
+import cors from 'cors';
 import { connectDb } from '@/db/connectDB';
 import { schemas } from '@/db/schema';
 import { resolvers } from '@/db/resolver';
@@ -10,6 +11,13 @@ const server = new ApolloServer({
   introspection:true,
 });
 connectDb()
-const handler = startServerAndCreateNextHandler(server);
+const handler = startServerAndCreateNextHandler(server, {context : async (req,res) => {
+  // Your additional context logic goes here
+
+  // Return the context object
+  return {
+   req,res,cors:cors()
+  };
+} });
 
 export { handler as GET, handler as POST };
