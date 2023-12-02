1f1ca872cf537f73a3a88c6e3d2e005332a1e3a6
Rajapandi <rajapandi12121997@gmail.com>
2023-12-03 01:57:57 +0530

    commit 2

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,23 +1,38 @@
 // import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+// import { startStandaloneServer } from '@apollo/server/standalone';
 import { connectDb } from '@/db/connectDB';
 import { schemas } from '@/db/schema';
 import { resolvers } from '@/db/resolver';
 
-const server = new ApolloServer({
+
+
+
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+
+interface MyContext {
+  token?: String;
+}
+const app = express();
+const httpServer = http.createServer(app);
+const server = new ApolloServer<MyContext>({
   resolvers:resolvers,
   typeDefs:schemas,
   introspection:true,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
-connectDb()
-const connectGQL = async () => {
-  const {url} =  await startStandaloneServer(server, {
+connectDb().then(async ()=>await server.start())
+.then(async () => await app.use(
+  '/api',
+  cors<cors.CorsRequest>(),
+  express.json(),
+  expressMiddleware(server, {
     context: async ({ req }) => ({ token: req.headers.token }),
-  });
-  console.log(`🚀  Server ready at ${url}`);
-}
-connectGQL()
-
-
-export { connectGQL as GET, connectGQL as POST };
+  }),
+)).then(async()=> await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve)))
+.then((res) => console.log(`🚀 Server ready at http://localhost:4000/api`,res))
+.catch((error)=>console.log("RP error:",error))
